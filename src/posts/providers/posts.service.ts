74e45ab994112c06d208e0d5aa043a1ac89256ab
surgvimon <surgvimon@gmail.com>
2024-12-22 19:35:31 +0700

    use Users-service -> inside Posts-service

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/providers/users.service';
 
 @Injectable()
 export class PostsService {
-    public findAll(userId: string) {
-      console.log(userId);
-    }
+  constructor(
+    /*
+     * Injecting Users Service
+     */
+    private readonly usersService: UsersService,
+  ) {}
+
+  public findAll(userId: string) {
+    const user = this.usersService.findOneById(userId);
+
+    return [
+      {
+        user: user,
+        title: 'Test Tile',
+        content: 'Test Content',
+      },
+      {
+        user: user,
+        title: 'Test Tile 2',
+        content: 'Test Content 2',
+      },
+    ];
   }
+}
