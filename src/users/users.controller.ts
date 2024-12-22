15f90e3939ebb21a360b6ab2ce37524c180d0058
surgvimon <surgvimon@gmail.com>
2024-12-22 17:57:45 +0700

    Injecting ->  Users Service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,9 +17,15 @@ import {
   import { CreateUserDto } from './dtos/create-user.dto';
   import { GetUsersParamDto } from './dtos/get-users-param.dto';
   import { PatchUserDto } from './dtos/patch-user.dto';
+  import { UsersService } from './providers/users.service';
   
   @Controller('users')
   export class UsersController {
+    constructor(
+        // Injecting Users Service
+        private readonly usersService: UsersService,
+    ) {}
+
     /**
      * Final Endpoint - /users/id?limit=10&page=1
      * Parama id - optional, convert to integer, cannot have a default value
@@ -37,20 +43,17 @@ import {
       @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
       @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
     ) {
-      console.log(limit);
-      console.log(page);
-      console.log(getUserParamDto);
-      return 'You sent a get request to users endpoint';
+        return this.usersService.findAll(getUserParamDto, limit, page);
     }
   
     @Post()
     public createUsers(@Body() createUserDto: CreateUserDto) {
-      console.log(createUserDto instanceof CreateUserDto);
-      return 'You sent a post request to users endpoint';
+        console.log(createUserDto instanceof CreateUserDto);
+        return 'You sent a post request to users endpoint';
     }
 
     @Patch()
     public patchUser(@Body() patchUserDto: PatchUserDto) {
         return patchUserDto;
-      }
+    }
   }
