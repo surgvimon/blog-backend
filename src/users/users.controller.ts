a060c36571e6882a4e51702aa1aafde047a8e737
surgvimon <surgvimon@gmail.com>
2024-12-22 15:58:07 +0700

    EP:3-1

---
+++ src/users/users.controller.ts
@@ -0,0 +1,52 @@
+import {
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    Put,
+    Query,
+    Body,
+    Headers,
+    Ip,
+    ParseIntPipe,
+    DefaultValuePipe,
+  } from '@nestjs/common';
+  
+  @Controller('users')
+  export class UsersController {
+    /**
+     * Final Endpoint - /users/id?limit=10&page=1
+     * Parama id - optional, convert to integer, cannot have a default value
+     * Query limit - integer, default 10
+     * Query page - integer, default value 1
+     * ==> USE CASES
+     * /users/ -> return all users with default pagination
+     * /users/1223 -> returns one user whos id is 1234
+     * /users?limit=10&page=2 -> return page 2 with limt of pagination 10
+     */
+  
+    @Get('/:id?')
+    public getUsers(
+      @Param('id', ParseIntPipe) id: number | undefined,
+      @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+      @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+    ) {
+      console.log(limit);
+      console.log(page);
+      return 'You sent a get request to users endpoint';
+    }
+  
+    @Post()
+    public createUsers(
+      @Body() request: any,
+      @Headers() headers: any,
+      @Ip() ip: any,
+    ) {
+      console.log(request);
+      console.log(headers);
+      console.log(ip);
+      return 'You sent a post request to users endpoint';
+    }
+  }
