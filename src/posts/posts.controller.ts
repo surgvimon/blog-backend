e011e82d5fac3a632baace81e485d0d7da8cc450
surgvimon <surgvimon@gmail.com>
2024-12-22 18:32:38 +0700

    create -> get posts rounte with DTO

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,12 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
 
 @Controller('posts')
 export class PostsController {
-    constructor(
-        /*
-         *  Injecting Posts Service
-         */
-        private readonly postsService: PostsService,
-    ) {}
+  constructor(
+    /*
+     *  Injecting Posts Service
+     */
+    private readonly postsService: PostsService,
+  ) {}
+
+  /*
+   * GET localhost:3000/posts/:userId
+   */
+  @Get('/:userId?')
+  public getPosts(@Param('userId') userId: string) {
+    return this.postsService.findAll(userId);
+  }
 }
