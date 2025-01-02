f5cdba31a4c9e86ae8f82c6c1eba4cdd57ee5fbc
surgvimon <surgvimon@gmail.com>
2025-01-02 20:59:57 +0700

    Post Entity

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -9,6 +9,7 @@ import {
   IsUrl,
   Matches,
   MinLength,
+  MaxLength,
   ValidateNested,
 } from 'class-validator';
 import { Type } from 'class-transformer';
@@ -18,25 +19,32 @@ import { CreatePostMetaOptionsDto } from './create-post-meta-options.dto';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreatePostDto {
-  @ApiProperty()
+  @ApiProperty({
+    example: 'This is a title',
+    description: 'This is the title for the blog post',
+  })
   @IsString()
   @MinLength(4)
+  @MaxLength(512)
   @IsNotEmpty()
   title: string;
 
   @ApiProperty({
     enum: PostType,
-    description: "Possible values  'post', 'page', 'story', 'series'",
+    description: "Possible values, 'post', 'page', 'story', 'series'",
   })
   @IsEnum(PostType)
   @IsNotEmpty()
   postType: PostType;
 
   @ApiProperty({
-    description: "For example 'my-url'",
+    description: "For Example - 'my-url'",
+    example: 'my-blog-post',
   })
   @IsString()
   @IsNotEmpty()
+  @MaxLength(256)
+  @MinLength(4)
   @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
     message:
       'A slug should be all small letters and uses only "-" and without spaces. For example "my-url"',
@@ -51,35 +59,48 @@ export class CreatePostDto {
   @IsNotEmpty()
   status: postStatus;
 
-  @ApiPropertyOptional()
-  @IsOptional()
+  @ApiPropertyOptional({
+    description: 'This is the content of the post',
+    example: 'The post content',
+  })
   @IsString()
+  @IsOptional()
   content?: string;
 
   @ApiPropertyOptional({
     description:
       'Serialize your JSON object else a validation error will be thrown',
+    example:
+      '{\r\n "@context": "https://schema.org",\r\n "@type": "Person"\r\n }',
   })
   @IsOptional()
   @IsJSON()
   schema?: string;
 
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    description: 'Featured image for your blog post',
+    example: 'http://localhost.com/images/image1.jpg',
+  })
   @IsOptional()
+  @MinLength(4)
+  @MaxLength(1024)
   @IsUrl()
   featuredImageUrl?: string;
 
-  @ApiProperty({
-    description: 'Must be a valid timestamp in ISO8601',
+  @ApiPropertyOptional({
+    description: 'The date on which the blog post is published',
     example: '2024-03-16T07:46:32+0000',
   })
   @IsISO8601()
   @IsOptional()
   publishOn?: Date;
 
-  @ApiPropertyOptional()
-  @IsArray()
+  @ApiPropertyOptional({
+    description: 'Array of tags passed as string values',
+    example: ['nestjs', 'typescript'],
+  })
   @IsOptional()
+  @IsArray()
   @IsString({ each: true })
   @MinLength(3, { each: true })
   tags?: string[];
@@ -92,9 +113,14 @@ export class CreatePostDto {
       properties: {
         key: {
           type: 'string',
+          description:
+            'The key can be any string identifier for your meta option',
+          example: 'sidebarEnabled',
         },
         value: {
-          type: 'string',
+          type: 'any',
+          description: 'Any value that you want to save to the key',
+          example: true,
         },
       },
     },
