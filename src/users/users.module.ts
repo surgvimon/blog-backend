74312357b7e513b3ecfe63be11af2e6af3c95572
surgvimon <surgvimon@gmail.com>
2025-01-02 19:38:03 +0700

    Frist Respository

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,14 @@ import { Module, forwardRef } from '@nestjs/common';
 import { AuthModule } from 'src/auth/auth.module';
 import { UsersController } from './users.controller';
 import { UsersService } from './providers/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
-  imports: [forwardRef(() => AuthModule)],
+  // imports: [forwardRef(() => AuthModule)],
+  imports: [TypeOrmModule.forFeature([User])],
 })
 export class UsersModule {}
